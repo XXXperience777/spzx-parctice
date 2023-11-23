9471c16f90da5f22004a33dc5209185dca163797
no135_lky <kaiyualiu2-c@my.cityu.edu.hk>
2023-11-23 10:13:33 +0800

    setup in win

---
+++ spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/IndexController.java
@@ -0,0 +1,11 @@
+package com.atguigu.spzx.manager.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "用户接口")
+@RestController
+@RequestMapping(value = "/admin/system/index")
+public class IndexController {
+}
